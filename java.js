c1a8de431c4c18bda336f764b7b1808554729569
rickpene <166026646+rickpene@users.noreply.github.com>
2025-07-16 01:21:27 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,4 +1,18 @@
- const hamburger = document.querySelector(".hamburger");
- const nav = document.querySelector(".nav");
- hamburger.addEventListener("click",() => nav.classList.toggle("active"));
- 
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+hamburger.addEventListener("click",() => nav.classList.toggle("active"));
+
+let lastScrollTop = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+if (currentScroll > lastScrollTop) {
+    // Rolando para baixo -> esconde a navbar
+    navbar.classList.add("hide");
+} else {
+    // Rolando para cima -> mostra a navbar
+    navbar.classList.remove("hide");
+}
+lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
